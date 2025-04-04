31ebf51929d19ae0b66450e5baa45a39e75140bf
szelenka <szelenka@gmail.com>
2025-04-03 22:53:28 -0400

    Restrict which Bluetooth devices are allowed to connect to C1-10P

---
+++ main/include/SettingsBluetooth.h
@@ -0,0 +1,16 @@
+#ifndef __SETTINGS_BLUETOOTH_H__
+#define __SETTINGS_BLUETOOTH_H__
+
+// JoyCon(L) Pink
+// #define C110P_CONTROLLER_LEFT_MAC_ADDRESS             "5C:52:1E:FF:6E:A2"
+
+// JoyCon(R) Green
+// #define C110P_CONTROLLER_RIGHT_MAC_ADDRESS            "5C:52:1E:FF:51:45"
+
+// JoyCon(L) White
+#define C110P_CONTROLLER_LEFT_MAC_ADDRESS           "98:E6:B9:62:6E:58"
+
+// JoyCon(R) Gray
+#define C110P_CONTROLLER_RIGHT_MAC_ADDRESS          "98:E6:B9:5A:CA:74"
+
+#endif
