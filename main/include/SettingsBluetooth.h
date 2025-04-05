d9b4353c81ae801edfb88b498aa05b9a4f2e2e81
szelenka <szelenka@gmail.com>
2025-04-05 13:24:12 -0400

    Update main/include/SettingsBluetooth.h
    Co-authored-by: FeGeZe <zelenka.felicity@gmail.com>

--- main/include/SettingsBluetooth.h
+++ main/include/SettingsBluetooth.h
@@ -2,15 +2,15 @@
 #define __SETTINGS_BLUETOOTH_H__
 
 // JoyCon(L) Pink
-// #define C110P_CONTROLLER_LEFT_MAC_ADDRESS             "5C:52:1E:FF:6E:A2"
+// #define C110P_CONTROLLER_LEFT_MAC_ADDRESS             "0A:00:0A:AA:0A:A0"
 
 // JoyCon(R) Green
-// #define C110P_CONTROLLER_RIGHT_MAC_ADDRESS            "5C:52:1E:FF:51:45"
+// #define C110P_CONTROLLER_RIGHT_MAC_ADDRESS            "0A:00:0A:AA:0A:A1"
 
 // JoyCon(L) White
-#define C110P_CONTROLLER_LEFT_MAC_ADDRESS           "98:E6:B9:62:6E:58"
+#define C110P_CONTROLLER_LEFT_MAC_ADDRESS           "0A:00:0A:AA:0A:A2"
 
 // JoyCon(R) Gray
-#define C110P_CONTROLLER_RIGHT_MAC_ADDRESS          "98:E6:B9:5A:CA:74"
+#define C110P_CONTROLLER_RIGHT_MAC_ADDRESS          "0A:00:0A:AA:0A:A3"
 
 #endif
