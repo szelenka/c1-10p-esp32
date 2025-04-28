180737883ce1673f772d41281efa3d0614d8c690
FeGeZe <zelenka.felicity@gmail.com>
2025-04-27 23:08:48 -0400

    Align Player LED with ControllerRole

--- main/include/chopper/core/ControllerRoles.h
+++ main/include/chopper/core/ControllerRoles.h
@@ -1,9 +1,10 @@
 #pragma once
 
+// align identifier with playerLED on controller for easy identifcation of role
 enum class ControllerRoles
 {
-    Drive,
-    Dome,
-    Animation,
-    Camera,
+    Drive = 1,      // 0001
+    Dome = 3,       // 0011
+    Animation = 7,  // 0111
+    Camera = 15,    // 1111
 };
